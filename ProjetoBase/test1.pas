128820e2e471a1438b1551a5207a73f50ef72b6b
lucas <lucas.axm@gmail.com>
2017-10-01 22:08:49 +0000

    exemplos adicionados

---
+++ ProjetoBase/test1.pas
@@ -0,0 +1,12 @@
+program testebool1(input, output);
+var b : boolean;
+   a  : integer;
+begin
+   b := a>10; 
+   b := true = true;
+   b := true = (a>10);
+   while (b) do
+      if (b) then
+         b:=false;
+   b:=1+2
+end.
