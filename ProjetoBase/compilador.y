1ad9d0f0e7bb97ea4f2045e91488feddbda2aa40
lucas <lucas.axm@gmail.com>
2017-10-10 22:06:57 +0000

    tokens adicionados. parte_declara_vars implementado. geraCodigo modificado.

--- ProjetoBase/compilador.y
+++ ProjetoBase/compilador.y
@@ -12,31 +12,55 @@
 
 int num_vars;
 
+void aloca_mem(){
+  if(num_vars>0){
+    char comando[5]="AMEM"; 
+    char *parametros[3];
+    // aloca variável aux pra converter num_vars pra string
+    char aux[10];
+    // converte num_vars pra string
+    snprintf(aux, sizeof(aux), "%d", num_vars);
+    // aloca memoria para primeiro param
+    parametros[0] = (char *) malloc(sizeof(char)*(strlen(aux)+1));
+    parametros[1] = NULL;
+    parametros[2] = NULL;
+    // armazena valor em parametro 1
+    strncpy(parametros[0], aux, sizeof(parametros[0]));
+    printf ("DEBUG | compilador.y | aloca_mem | num_vars: %d / comando: \"%s\" / parametros[0]: \"%s\"\n", num_vars, comando, parametros[0]);
+    num_vars=0; // zera contador de variáveis
+    geraCodigo (NULL, comando, parametros);
+  }  
+}
+
 %}
 
 %token PROGRAM ABRE_PARENTESES FECHA_PARENTESES 
 %token VIRGULA PONTO_E_VIRGULA DOIS_PONTOS PONTO
 %token T_BEGIN T_END VAR IDENT ATRIBUICAO
+%token LABEL PROCEDURE FUNCTION GOTO IF THEN ELSE WHILE
 
 %%
 
-programa    :{ 
-             geraCodigo (NULL, "INPP"); 
-             }
-             PROGRAM IDENT 
-             ABRE_PARENTESES lista_idents FECHA_PARENTESES PONTO_E_VIRGULA
-             bloco PONTO {
-             geraCodigo (NULL, "PARA"); 
-             }
+programa :
+    {
+        num_vars=0;
+        geraCodigo (NULL, "INPP", NULL); 
+    }
+    PROGRAM IDENT 
+    ABRE_PARENTESES lista_idents FECHA_PARENTESES PONTO_E_VIRGULA
+    bloco PONTO
+    {
+        geraCodigo (NULL, "PARA", NULL); 
+    }
 ;
 
-bloco       : 
-              parte_declara_vars
-              { 
-              }
-
-              comando_composto 
-              ;
+bloco : 
+    parte_declara_vars
+    {
+        aloca_mem();
+    }
+    comando_composto 
+;
 
 
 
@@ -64,9 +88,15 @@ declara_var : { }
 tipo        : IDENT
 ;
 
-lista_id_var: lista_id_var VIRGULA IDENT 
-              { /* insere última vars na tabela de símbolos */ }
-            | IDENT { /* insere vars na tabela de símbolos */}
+lista_id_var:
+    lista_id_var VIRGULA IDENT 
+    { /* insere última var na tabela de símbolos */ 
+        num_vars++;
+    }
+    | IDENT
+    { /* insere vars na tabela de símbolos */
+        num_vars++;
+    }
 ;
 
 lista_idents: lista_idents VIRGULA IDENT  
--- ProjetoBase/compiladorF.c
+++ ProjetoBase/compiladorF.c
@@ -23,16 +23,48 @@
  * ------------------------------------------------------------------- */
 
 FILE* fp=NULL;
-void geraCodigo (char* rot, char* comando) {
 
+void geraCodigo (char* rot, char* comando, char **parametros) {
+  
+  // aloca espaco para comando + \0 em comando_completo
+  char *comando_completo = (char *) malloc (sizeof(char)*5);
+  // adiciona comando em comando_completo
+  snprintf (comando_completo, sizeof(comando_completo), "%s", comando);
+  printf ("DEBUG | compiladorF.c | geraCodigo | comando: \"%s\"\n", comando);
+  
   if (fp == NULL) {
     fp = fopen ("MEPA", "w");
   }
-
+  
+  if (parametros!= NULL && parametros[0]!=NULL){
+    // aloca memoria para 1 parametro + 1 espaco
+    comando_completo = (char *) realloc (comando_completo, sizeof(char)*(sizeof(comando_completo)+strlen(parametros[0])+1));
+    snprintf (comando_completo, sizeof(comando_completo), "%s %s", comando, parametros[0]);
+    // comando_completo = "comando parametros[0]"
+    printf ("DEBUG | compiladorF.c | geraCodigo | parametros[0]: \"%s\"\n", parametros[0]);
+    if (parametros[1]!=NULL){
+      // aloca memoria para + 1 virgula + 1 espaco + 1 parametro
+      comando_completo = (char *) realloc (comando_completo, sizeof(char)*(sizeof(comando_completo)+strlen(parametros[1])+2));
+      snprintf (comando_completo, sizeof(comando_completo), "%s %s, %s", comando, parametros[0], parametros[1]);
+      // comando_completo = "comando parametros[0], parametros[1]"
+      printf ("DEBUG | compiladorF.c | geraCodigo | parametros[1]: \"%s\"\n", parametros[1]);
+      if (parametros[2]!=NULL){
+        // aloca memoria para + 1 virgula + 1 espaco + 1 parametro
+        comando_completo = (char *) realloc (comando_completo, sizeof(char)*(sizeof(comando_completo)+strlen(parametros[2])+2));
+        snprintf (comando_completo, sizeof(comando_completo), "%s %s, %s, %s", comando, parametros[0], parametros[1], parametros[2]);
+        // comando_completo = "comando parametros[0], parametros[1], parametros[2]"
+        printf ("DEBUG | compiladorF.c | geraCodigo | parametros[2]: \"%s\"\n", parametros[2]);
+      }
+    }
+  }
+  
+  
   if ( rot == NULL ) {
-    fprintf(fp, "     %s\n", comando); fflush(fp);
+    printf ("DEBUG | compiladorF.c | geraCodigo | comando_completo: \"%s\"\n", comando_completo);
+    fprintf(fp, "     %s\n", comando_completo); fflush(fp);
   } else {
-    fprintf(fp, "%s: %s \n", rot, comando); fflush(fp);
+    printf ("DEBUG | compiladorF.c | geraCodigo | rot+comando_completo: \"%s: %s\"\n", rot, comando_completo);
+    fprintf(fp, "%s: %s \n", rot, comando_completo); fflush(fp);
   }
 }
 
